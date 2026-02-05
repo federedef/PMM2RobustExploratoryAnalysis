7e7785c3f0825d834b21d087b9f0d16bbf482a33
federedef <federogc98@gmail.com>
2026-02-05 15:52:29 +0100

    Improving workflow presentation and organization

--- README.md
+++ README.md
@@ -1,1 +1,125 @@
-# PMM2RobustExploratoryAnalysis
+# PMM2 Robust Exploratory Analysis Workflow
+
+This repository contains a computational workflow developed for the article on PMM2-related disorders.
+The goal of this workflow is to perform a robust exploratory analysis on multi-omics and clinical data
+from patients with PMM2 disease, with the aim of identifying potential biomarkers that discriminate
+between patients with high severity and low severity phenotypes.
+
+The workflow integrates phenotypic, genetic, transcriptomic, miRNA, and metabolomic data and applies
+a multifactorial analysis framework followed by posterior analyses to explore severity-associated
+patterns in the data.
+
+--------------------------------------------------
+OVERVIEW
+--------------------------------------------------
+
+The workflow is organized into four main stages:
+
+1. Environment setup
+2. Dataset parsing and preprocessing
+3. Multifactor (robust exploratory) analysis
+4. Posterior analysis and result inspection
+
+Parsed datasets and intermediate results are generated locally and are not intended to be versioned.
+
+--------------------------------------------------
+REQUIREMENTS
+--------------------------------------------------
+
+- Bash-compatible shell
+- Python (with venv support)
+- R (with required annotation scripts available in the environment)
+- External tools available in the environment:
+  - semtools
+  - stable_select
+  - AutoFlow
+
+Environment-specific initialization scripts are assumed to be available and correctly configured.
+
+--------------------------------------------------
+DATA STRUCTURE
+--------------------------------------------------
+
+The input datasets required to run this workflow are not publicly distributed due to data privacy constraints.
+
+A separate **private repository** containing the patient datasets is associated with this project and is
+integrated into the main repository as a **git submodule**. Users must have explicit access permissions
+to this private repository in order to run the workflow.
+
+Once access is granted, the private dataset repository is expected to be available locally under a
+folder named `datasets`.
+
+Parsed and intermediate datasets generated during execution are created locally in a separate input
+folder and are not intended to be versioned or shared.
+
+--------------------------------------------------
+WORKFLOW EXECUTION
+--------------------------------------------------
+
+The workflow is controlled via a single script with different execution modes.
+
+1) Environment setup
+
+This step creates a Python virtual environment and installs required dependencies.
+
+Command:
+./workflow.sh set_env
+
+Note:
+This step must be executed once before running the parsing stage.
+
+--------------------------------------------------
+
+2) Dataset parsing and preprocessing
+
+This step parses and preprocesses all datasets, generating normalized and annotated versions
+of phenotypes, genes, miRNAs, and metabolomic data for both full and sample cohorts.
+
+Command:
+./workflow.sh parse
+
+Requirements:
+- The virtual environment must exist
+- The `datasets` folder must be present and correctly populated
+
+--------------------------------------------------
+
+3) Multifactor analysis (robust exploratory analysis)
+
+This step performs the core multifactorial exploratory analysis aimed at identifying
+patterns associated with disease severity.
+
+Command:
+./workflow.sh ma [options]
+
+This stage uses AutoFlow to execute the multifactor analysis workflow.
+
+--------------------------------------------------
+
+4) Posterior analysis
+
+This step performs posterior analyses based on the results of the multifactor analysis,
+allowing further interpretation and refinement of severity-associated signals.
+
+Command:
+./workflow.sh pa [options]
+
+--------------------------------------------------
+
+5) Workflow monitoring and recovery
+
+Check the status of running or completed analyses:
+./workflow.sh check
+
+Recover or resume interrupted analyses:
+./workflow.sh recover
+
+--------------------------------------------------
+NOTES
+--------------------------------------------------
+
+- Parsed datasets are generated locally and should not be committed to the repository.
+- The workflow is designed for exploratory and hypothesis-generating analyses.
+- Results should be interpreted in the context of PMM2 disease biology and validated independently.
+
+--------------------------------------------------
--- launch.sh
+++ launch.sh
@@ -5,59 +5,87 @@ export EXEC_PATH=$FSCRATCH/PMM2REA
 export CODE_PATH=$CURRENT_PATH/aux_scripts
 export TEMPLATES_PATH=$CURRENT_PATH/templates
 export PATH=$CODE_PATH:$PATH
-datasets_folder=$CURRENT_PATH/datasets
-blacklist="get_synth_data_from_hunter,generate_degs,generate_report_PCA_synth_degs"
-blacklist="get_synth_data_from_hunter"
+datasets_folder=$CURRENT_PATH/datasets/datasets
+input_folder=$CURRENT_PATH/parsed_datasets
+seed=123
 
-# Parsing inputs
+if [ $1 == "set_env" ] ; then
+    if [ ! -d venv ] ; then
+        python -m venv venv --system-site-packages
+        source ./venv/bin/activate
+        pip install -e ~/dev_py/py_exp_calc
+        echo -e "environment was set up, please run parse option now"
+        exit 0
+    fi
+fi
 
 if [ $1 == "parse" ] ; then
+    # Parsing inputs
     source ~soft_bio_267/initializes/init_python
     source ~/dev_R/ExpHunterSuite/init_degenes_hunter
     if [ ! -d venv ] ; then
-            python -m venv venv --system-site-packages
-            source ./venv/bin/activate
-            pip install -e ~/dev_py/py_exp_calc
-            exit 0
+        echo -e "Please run set_env option"
+        exit 0
     fi
     source ./venv/bin/activate
-    
     if [ ! -d datasets ] ; then
         echo -e "Error: You need to create a folder named datasets with"\
                 "all datasets needed for the analysis.\n" \
                 "These include: phenotypes, miRNA, gene and metabolic data"
     fi
-    pushd ./datasets/sample_cohort
-    echo -e "Parsing phenotypes\n"
-    # head -n 1 phenotypes.txt | tr "\t" "\n"|semtools -i - -O HPO --list_translate names \
-    # | cut -f 2 | tr "\n" "\t" | sed -r 's/\t$/\n/g' > phenotypes_n.txt
-    # tail -n +2 phenotypes.txt >> phenotypes_n.txt
+    # Parsing all cohort
+    data_path=$datasets_folder/all_cohort
+    parsed_path=$input_folder/all_cohort
+    mkdir -p $parsed_path
 
-    echo -e "Parsing genes\n"
-    # echo -e "The number of total genes are:"
-    # number_of_total_genes=`head -n 1 all_genes_var_filtered.txt | tr -s "\t" "\n" | wc -l`
-    # echo -e "$number_of_total_genes"
-    # stable_select -i all_genes_var_filtered.txt --label label_genes.txt -o genes.txt
-    # number_of_selected_genes=`head -n 1 genes.txt | tr -s "\t" "\n" | wc -l`
-    # echo -e "The number of selected genes are: $number_of_selected_genes"
-    sed -i "s/^\t//" genes.txt
-    head -n 1 genes.txt | tr "\t" "\n" > gene_list.txt
-    sed -i '1s/^/ENSEMBL\n/' gene_list.txt
-    add_annotation.R -i gene_list.txt -o genes_tr.txt -c 1 -I ENSEMBL -K SYMBOL
-    tail -n +2 genes_tr.txt | awk '{if ($2 == "NA") print $1; else print $2 }'  | tr "\n" "\t"| sed -r 's/\t$/\n/g' > genes_n.txt
-    tail -n +2 genes.txt >> genes_n.txt
+    ln -s $data_path/phenotypes.txt $parsed_path/phenotypes.txt
+    head -n 1 $parsed_path/phenotypes.txt | tr "\t" "\n"|semtools -i - -O HPO --list_translate names \
+    | cut -f 2 | tr "\n" "\t" | sed -r 's/\t$/\n/g' > $parsed_path/phenotypes_n.txt
+    tail -n +2 $parsed_path/phenotypes.txt >> $parsed_path/phenotypes_n.txt
 
-    # head -n 1 miRNA.txt | tr "\t" "\n" > miRNA_list.txt
-    # sed -i '1s/^/MIMAT\n/' miRNA_list.txt
-    # add_annotation.R -i miRNA_list.txt -o miRNA_tr.txt -c 1 -m
-    # tail -n +2 miRNA_tr.txt | awk '{if ($2 == "NA") print $1; else print $2 }'  | tr "\n" "\t"| sed -r 's/\t$/\n/g' > miRNA_n.txt
-    # tail -n +2 miRNA.txt >> miRNA_n.txt
-    popd
+    # Parsing sample cohort
+    data_path=$datasets_folder/sample_cohort
+    parsed_path=$input_folder/sample_cohort
+    mkdir -p $parsed_path
 
-    pushd ./datasets/all_cohort
-    head -n 1 phenotypes.txt | tr "\t" "\n"|semtools -i - -O HPO --list_translate names | cut -f 2 | tr "\n" "\t" | sed -r 's/\t$/\n/g' > phenotypes_n.txt
-    tail -n +2 phenotypes.txt >> phenotypes_n.txt
-    popd
+    # Phenotypes
+    echo -e "Parsing phenotypes:\n"
+    ln -s $data_path/phenotypes.txt $parsed_path/phenotypes.txt
+    head -n 1 $parsed_path/phenotypes.txt | tr "\t" "\n"|semtools -i - -O HPO --list_translate names \
+     | cut -f 2 | tr "\n" "\t" | sed -r 's/\t$/\n/g' > $parsed_path/phenotypes_n.txt
+     tail -n +2 $parsed_path/phenotypes.txt >> $parsed_path/phenotypes_n.txt
+    # Genes
+    echo -e "Parsing genes:\n"
+    ln -s $data_path/all_genes_var_filtered.txt $parsed_path/all_genes_var_filtered.txt
+    ln -s $data_path/label_genes.txt $parsed_path/label_genes.txt
+    echo -e "The number of total genes are:"
+    number_of_total_genes=`head -n 1 $parsed_path/all_genes_var_filtered.txt | tr -s "\t" "\n" | wc -l`
+    echo -e "$number_of_total_genes"
+    stable_select -i $parsed_path/all_genes_var_filtered.txt --label $parsed_path/label_genes.txt -o $parsed_path/genes.txt
+    number_of_selected_genes=`head -n 1 $parsed_path/genes.txt | tr -s "\t" "\n" | wc -l`
+    echo -e "The number of selected genes are: $number_of_selected_genes"
+    sed -i "s/^\t//" $parsed_path/genes.txt
+    head -n 1 $parsed_path/genes.txt | tr "\t" "\n" > $parsed_path/gene_list.txt
+    sed -i '1s/^/ENSEMBL\n/' $parsed_path/gene_list.txt
+    add_annotation.R -i $parsed_path/gene_list.txt -o $parsed_path/genes_tr.txt -c 1 -I ENSEMBL -K SYMBOL
+    tail -n +2 $parsed_path/genes_tr.txt | awk '{if ($2 == "NA") print $1; else print $2 }'  \
+    | tr "\n" "\t"| sed -r 's/\t$/\n/g' > $parsed_path/genes_n.txt
+    tail -n +2 $parsed_path/genes.txt >> $parsed_path/genes_n.txt
+    # miRNA
+    echo -e "Parsing miRNAs:\n"
+    ln -s $data_path/miRNA.txt $parsed_path/miRNA.txt
+    head -n 1 $parsed_path/miRNA.txt | tr "\t" "\n" > $parsed_path/miRNA_list.txt
+    sed -i '1s/^/MIMAT\n/' $parsed_path/miRNA_list.txt
+    add_annotation.R -i $parsed_path/miRNA_list.txt -o $parsed_path/miRNA_tr.txt -c 1 -m
+    tail -n +2 $parsed_path/miRNA_tr.txt | awk '{if ($2 == "NA") print $1; else print $2 }' \
+    | tr "\n" "\t"| sed -r 's/\t$/\n/g' > $parsed_path/miRNA_n.txt
+    tail -n +2 $parsed_path/miRNA.txt >> $parsed_path/miRNA_n.txt
+    # metabolomics
+    ln -s $data_path/metabolomics.txt $parsed_path/metabolomics.txt
+    # Supp
+    for file in severity_scales.txt severity.txt variants.txt ; do 
+        ln -s $data_path/$file $parsed_path/$file
+    done
 fi
 
 if [ $1 == "ma" ] ; then
@@ -66,8 +94,8 @@ if [ $1 == "ma" ] ; then
     path_to_autoflow_exec=$EXEC_PATH/multifactor_analysis
     mkdir -p $path_to_autoflow_exec
 	variables=`echo -e "
-		\\$datasets=$datasets_folder,
-        \\$seed=123
+		\\$datasets=$input_folder,
+        \\$seed=$seed
 	" | tr -d [:space:]`
 	AutoFlow -e -w $TEMPLATES_PATH/multifactor_analysis.af -V $variables -o $path_to_autoflow_exec -c 1 -m 60gb -t 0-02:30:00 -n cal $2
 fi
@@ -79,9 +107,9 @@ if [ $1 == "pa" ] ; then
     results_from_ma=$CURRENT_PATH/PCA_top_results #`grep -w "collect_results" $EXEC_PATH/multifactor_analysis/index_execution | cut -f 2`
     mkdir -p $path_to_autoflow_exec
 	variables=`echo -e "
-		\\$datasets=$datasets_folder,
+		\\$datasets=$input_folder,
         \\$results_from_ma=$results_from_ma,
-        \\$seed=123
+        \\$seed=$seed
 	" | tr -d [:space:]`
 	AutoFlow -e -w $TEMPLATES_PATH/posterior_analysis.af -V $variables -o $path_to_autoflow_exec -c 1 -m 20gb -t 0-02:00:00 -n cal $2
 fi
